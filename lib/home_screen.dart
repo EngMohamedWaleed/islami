1952017955bcb6b21cda4da1c218b62b2c1e0a1c
EngMohamedWaleed <mb1972002@gmail.com>
2022-12-03 23:05:01 +0200

    create splash screen add images

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String routeName = 'home';
-
+//devlmgmll
   @override
   Widget build(BuildContext context) {
     return Scaffold(
